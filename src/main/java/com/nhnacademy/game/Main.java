718dfeffdbc2696aa8edc5cd5be080b1eb5168de
phh624 <phh624@hanmail.net>
2024-10-11 15:50:24 +0900

    Lab2_7

--- src/main/java/com/nhnacademy/game/Main.java
+++ src/main/java/com/nhnacademy/game/Main.java
@@ -1,22 +1,36 @@
 package com.nhnacademy.game;
 
-import java.awt.Graphics;
+import java.awt.Color;
+import java.util.Random;
 
 import javax.swing.JFrame;
 
-public class Main extends JFrame {
-    PaintableBall ball = new PaintableBall(100, 100, 50);
-
-    @Override
-    public void paint(Graphics g) {
-        super.paint(g);
-        ball.paint(g);
-    }
-
+public class Main {
     public static void main(String[] args) {
-        JFrame frame = new Main();
+        Color[] colors = new Color[] { Color.BLUE, Color.RED, Color.WHITE, Color.BLACK, Color.GREEN };
 
-        frame.setSize(300, 200);
+        Random random = new Random();
+        JFrame frame = new JFrame();
+
+        frame.setSize(500, 400);
+
+        World world = new World();
+        frame.add(world);
         frame.setVisible(true);
+
+        for (int i = 0; i < 10; i++) {
+            int x = random.nextInt(500);
+            int y = random.nextInt(400);
+            int radius = 10 + random.nextInt(41);
+
+            if (random.nextBoolean()) {
+                Color color = colors[random.nextInt(colors.length)];
+                world.add(new PaintableBall(x, y, radius, color));
+            } else {
+                world.add(new Ball(x, y, radius));
+            }
+        }
+
+        frame.repaint();
     }
 }
