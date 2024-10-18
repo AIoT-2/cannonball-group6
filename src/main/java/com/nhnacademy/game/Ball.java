781267ff61ee200526585762a754b7496dc142e0
saumonrose <wjdwldus0809@naver.com>
2024-10-18 14:21:23 +0900

    Lab2-1

---
+++ src/main/java/com/nhnacademy/game/Ball.java
@@ -0,0 +1,44 @@
+public class Ball {
+
+    /**
+     * @param x
+     * @param y
+     * @param radius
+     */
+    public Ball(int x, int y, int radius) {
+        //
+    }
+
+    /**
+     * 
+     * @return ball의 x축 상의 위치
+     */
+    public int getX() {
+        //
+    }
+
+    /**
+     * 
+     * @return ball의 y축 상의 위치
+     */
+    public int getY() {
+        //
+    }
+
+    /**
+     * @return ball의 반지름을 반환한다.
+     */
+    public int getRadius() {
+        //
+    }
+
+    /**
+     * @return ball의 정보를 문자열로 변환해 반환한다.
+     */
+
+    @Override
+    public String toString() {
+
+    }
+
+}
