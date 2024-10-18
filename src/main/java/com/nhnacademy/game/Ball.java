58ed79dccfa1fdee7af731584517131c9d529f24
saumonrose <wjdwldus0809@naver.com>
2024-10-18 15:38:43 +0900

    Lab2-2

--- src/main/java/com/nhnacademy/game/Ball.java
+++ src/main/java/com/nhnacademy/game/Ball.java
@@ -1,4 +1,7 @@
 public class Ball {
+`   int x;
+    int y;
+    int radius;
 
     /**
      * @param x
@@ -6,7 +9,9 @@ public class Ball {
      * @param radius
      */
     public Ball(int x, int y, int radius) {
-        //
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
     }
 
     /**
@@ -32,13 +37,65 @@ public class Ball {
         //
     }
 
+
+    /**
+     * @return ball의 영역 중 최소 x좌표 값
+     */
+    public int getMinX(){
+//
+    }
+
+
+    /**
+     * 
+     * @return ball의 영역 중 최대 x좌표 값
+     */
+    public int getMaxX(){
+        //
+    }
+
+    /**
+     * 
+     * @return ball의 영역 중 최소 y좌표 값
+     */
+    public int getMinY(){
+        //코드 추가
+    }
+
+    /**
+     * 
+     * @return ball의 영역 중 최대 y좌표 값
+     */
+    public int getMaxY(){
+        // 코드 추가
+    }
+
+
+    /**
+     * 
+     * @return ball의 영역 넓이(x축의 길이)
+     */
+    public int getWidth(){
+        //코드 추가
+    }
+
+
+    /**
+     * 
+     * @return ball의 영역 높이(y축의 길이)
+     */
+    public int getHeight(){
+        //코드 추가
+    }
+
+
     /**
      * @return ball의 정보를 문자열로 변환해 반환한다.
      */
 
     @Override
     public String toString() {
-
+        return String.format("[(%d, %d),%d]", x, y, radius);
     }
 
 }
